a47d77d2723028f780c8daf4f6f33869d21a1238
Luke Policinski <Luke@LukePOLO.com>
2017-08-31 19:45:58 +0000

    Apply fixes from StyleCI

--- src/Commands/ApiMigrationMakeCommand.php
+++ src/Commands/ApiMigrationMakeCommand.php
@@ -5,7 +5,6 @@ namespace LukePOLO\LaravelApiMigrations\Commands;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\File;
 use Illuminate\Console\GeneratorCommand;
-use function is_numeric;
 use LukePOLO\LaravelApiMigrations\ServiceProvider;
 
 class ApiMigrationMakeCommand extends GeneratorCommand
@@ -48,7 +47,7 @@ class ApiMigrationMakeCommand extends GeneratorCommand
             $this->version = $this->ask('Please enter a version number :', $this->apiDetails->keys()->count() + 1);
         }
 
-        if(!is_numeric($this->version)) {
+        if (! is_numeric($this->version)) {
             $this->error('You provided a invalid version number');
 
             return false;
